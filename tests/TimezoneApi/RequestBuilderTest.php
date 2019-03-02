e299bc2b6e0b86e238d5c8322a355480e9d20c1c
Franck <franck.schmitt@hotmail.com>
2019-03-02 18:55:41 +0100

    Improve unit tests

---
+++ tests/TimezoneApi/RequestBuilderTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\GoogleMapsClient\TimezoneApi;
+
+use GoogleMapsClient\GoogleMapsRequest;
+use GoogleMapsClient\Language;
+use GoogleMapsClient\TimezoneApi\TimezoneLocation;
+
+class RequestBuilderTest extends \PHPUnit\Framework\TestCase
+{
+    public function testClientUsage(): void
+    {
+        $request = GoogleMapsRequest::newTimezoneRequest(
+            new TimezoneLocation('39.6034810', '-119.6822510'), 1331161200
+        )->withLanguage(Language::CZECH());
+
+        $generatedUri = urldecode($request->getRequest()->getUri()->__toString());
+        $expectedUri = '/location=39.6034810,-119.6822510&timestamp=1331161200&language=cs';
+
+        $this->assertSame($expectedUri, $generatedUri);
+    }
+}
